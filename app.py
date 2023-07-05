4da1df6f4ee2d0f2a114a0d16b38ae24010ee48f
Hatim <hatmsb11@gmail.com>
2023-07-05 16:54:22 +0530

    phase 2

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route("/dashboard")
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable = False)
+    desc = db.Column(db.String(500), nullable = False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+@app.route("/")
 def dashboard():
+    todo = Todo(title="My first task",desc="Start doing flask!!")
+    db.session.add(todo)
+    db.session.commit()
     return render_template("home.html")
 
 @app.route("/task")
 def task():
-    return render_template("home2.html")
+    return render_template("addTodo.html")
 
 
 if  __name__=="__main__":
